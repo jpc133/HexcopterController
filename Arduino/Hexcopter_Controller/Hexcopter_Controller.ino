5710f18e6459c8335f7d45824662c239fe27407c
Jonathan Clarke <jonathan.clarke73@gmail.com>
2021-11-24 20:52:34 +0000

    Move files again and start updating code to get ready for added raspPi

--- Arduino/Hexcopter_Controller/Hexcopter_Controller.ino
+++ Arduino/Hexcopter_Controller/Hexcopter_Controller.ino
@@ -26,19 +26,21 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 //Define Motor array
 #define MOTORS_AVAILABLE 6 //This only works with Hexcopters with 60 degrees between arms with an arm straight forward and back
 HexMotor motors[MOTORS_AVAILABLE] = {};
-//See diagram below of motor layout
-//       5  L  6
-//        \   /
-//BACK  4 -BBB- 1  FRONT
-//        /   \
-//       3  R  2
-//Motor Positions: ([+/-]sin(60),[+/-]sin(60)) and also (0,1) and (0,-1)
-//1 - (0,1)
-//3 - (0.866,0.5)
-//3 - (0.866,-0.5)
-//4 - (0,-1)
-//5 - (-0.866,-0.5)
-//6 - (-0.866,0.5)
+/*
+See diagram below of motor layout
+       5  L  6
+        \   /
+BACK  4 -BBB- 1  FRONT
+        /   \
+       3  R  2
+Motor Positions: ([+/-]sin(60),[+/-]sin(60)) and also (0,1) and (0,-1)
+1 - (0,1)
+3 - (0.866,0.5)
+3 - (0.866,-0.5)
+4 - (0,-1)
+5 - (-0.866,-0.5)
+6 - (-0.866,0.5)
+*/
 
 //60Deg × π/180 = 1.047Rad
 double positive_Motor_X_Offset = sin(60 * (PI / 180)); //Approx 0.866
@@ -58,13 +60,6 @@ Madgwick filter;
 unsigned long microsPerReading, microsPrevious;
 float accelScale, gyroScale;
 
-BLEPeripheral blePeripheral; // create peripheral instance
-BLEService ledService("19B10010-E8F2-537E-4F6C-D104768A1214");
-
-BLELongCharacteristic GyroXCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-BLELongCharacteristic GryoYCharacteristic("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-BLELongCharacteristic GryoZCharacteristic("19B10014-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
-
 void setup() {
   Serial.begin(9600);
 
@@ -74,11 +69,6 @@ void setup() {
   motors[3] = {4, 4, true, 0.0,-1.0};
   motors[4] = {5, 5, false, -positive_Motor_X_Offset,-positive_Motor_Y_Offset};
   motors[5] = {6, 6, true, -positive_Motor_X_Offset,positive_Motor_Y_Offset};
-
-   for(uint16_t i = 1; i <= MOTORS_AVAILABLE; i++) {  
-    Serial.print("Motor" + i);
-    Serial.print("    ");  
-  }
  
   // start the IMU and filter
   CurieIMU.begin();
@@ -90,23 +80,6 @@ void setup() {
   CurieIMU.setAccelerometerRange(2);
   // Set the gyroscope range to 250 degrees/second
   CurieIMU.setGyroRange(250);
-
-  //Bluetooth Setup
-  // set the local name peripheral advertises
-  blePeripheral.setLocalName("Test 101 BLE");
-  // set the UUID for the service this peripheral advertises:
-  blePeripheral.setAdvertisedServiceUuid(ledService.uuid());
-
-  //Add attributes for bluetooth
-  blePeripheral.addAttribute(ledService);
-  blePeripheral.addAttribute(GyroXCharacteristic);
-  blePeripheral.addAttribute(GryoYCharacteristic);
-  blePeripheral.addAttribute(GryoZCharacteristic);
-
-  //Set Bluetooth characteristics
-  GyroXCharacteristic.setValue(0.0f);
-  GryoYCharacteristic.setValue(0.0f);
-  GryoZCharacteristic.setValue(0.0f);
 
   pwm.begin();
 
@@ -139,9 +112,6 @@ void setup() {
   for(uint16_t i = 1; i <= MOTORS_AVAILABLE; i++) {
     pwm.setPWM(i, 0, PWM_MIN);
   }
-  
-  // advertise the bluetooth
-  blePeripheral.begin();
 }
 
 void loop() {
@@ -169,18 +139,21 @@ void loop() {
   roll = filter.getRoll();
   pitch = filter.getPitch();
   heading = filter.getYaw();
-  /*Serial.print("Orientation: ");
+  Serial.print("{orientation: [");
   Serial.print(heading);
-  Serial.print(" ");
+  Serial.print(",");
   Serial.print(pitch);
-  Serial.print(" ");
-  Serial.println(roll);*/
+  Serial.print(",");
+  Serial.print(roll);
+  Serial.print("], controlValues: [");
+
+  //Timeout on pulseIn, microseconds
+  #define PULSE_IN_TIMEOUT 25000
 
   //Throttle
-  pwm_throttle = pulseIn(THROTTLE_IN_PIN, HIGH);
-  //Serial.print("Throttle: ");
-  //Serial.print(pwm_throttle);
-  //Serial.print("  ");
+  pwm_throttle = pulseIn(THROTTLE_IN_PIN, HIGH, PULSE_IN_TIMEOUT);
+  Serial.print(pwm_throttle);
+  Serial.print(",");
   double pwm_throttle_converted = ((pwm_throttle - 1069.0)/850.0);
   //Serial.println(pwm_throttle_converted);
   if(pwm_throttle < 900){
@@ -188,10 +161,9 @@ void loop() {
   }
 
   //Roll
-  pwm_roll = pulseIn(ROLL_IN_PIN, HIGH);
-  //Serial.print("Roll: ");
-  //Serial.print(pwm_roll);
-  //Serial.print("  ");
+  pwm_roll = pulseIn(ROLL_IN_PIN, HIGH, PULSE_IN_TIMEOUT);
+  Serial.print(pwm_roll);
+  Serial.print(",");
   double pwm_roll_converted = (((pwm_roll - 1069.0)/850.0) * 2)-1;
   //Serial.println(pwm_roll_converted);
   if(pwm_roll < 900){
@@ -199,10 +171,9 @@ void loop() {
   }
 
   //Pitch
-  pwm_pitch = pulseIn(PITCH_IN_PIN, HIGH);
-  //Serial.print("Pitch: ");
-  //Serial.print(pwm_pitch);
-  //Serial.print("  ");
+  pwm_pitch = pulseIn(PITCH_IN_PIN, HIGH, PULSE_IN_TIMEOUT);
+  Serial.print(pwm_pitch);
+  Serial.print(",");
   double pwm_pitch_converted = (((pwm_pitch - 1069.0)/850.0) * 2)-1;
   //Serial.println(pwm_pitch_converted);
   if(pwm_pitch < 900){
@@ -210,18 +181,13 @@ void loop() {
   }
 
   //Yaw
-  pwm_yaw = pulseIn(YAW_IN_PIN, HIGH);
-  //Serial.print("Yaw: ");
-  //Serial.print(pwm_yaw);
-  //Serial.print("  ");
+  pwm_yaw = pulseIn(YAW_IN_PIN, HIGH, PULSE_IN_TIMEOUT);
+  Serial.print(pwm_yaw);
+  Serial.print("]}");
   double pwm_yaw_converted = (((pwm_yaw - 1069.0)/850.0) * 2)-1;
   if(pwm_yaw < 900){
     pwm_yaw_converted = 0;
   }
-
-  GyroXCharacteristic.setValue(roll);
-  GryoYCharacteristic.setValue(pitch);
-  GryoZCharacteristic.setValue(heading);
   
   double motorSpeeds[MOTORS_AVAILABLE] = {};
   double maxSpeed = 0;
@@ -239,8 +205,8 @@ void loop() {
     else{
       motorSpeeds[i-1] = 0;
     }
-    Serial.print(precision_map(motorSpeeds[i-1],0.0,1.0,PWM_MIN,PWM_MAX));
-    Serial.print("    ");  
+    //Serial.print(precision_map(motorSpeeds[i-1],0.0,1.0,PWM_MIN,PWM_MAX));
+    //Serial.print("    ");  
     pwm.setPWM(i, 0, precision_map(motorSpeeds[i-1],0.0,1.0,PWM_MIN,PWM_MAX));
   }
   Serial.println("");
